4da64d7daf212faf461672be7b5d46d9cdbedb11
nathan <nathannash11@gmail.com>
2020-01-31 21:39:29 +0000

    basic gameplay

--- js/SoundBox.js
+++ js/SoundBox.js
@@ -1,8 +1,40 @@
 class SoundBox{
 
-    constructor(){
-        
+    constructor(x, word){
+        this.sound = new Howl({
+            src: ['../audio/sound2.webm', '../audio/sound2.mp3'],
+            sprite: {
+                one: [0, 450, true],
+                two: [2000, 250, true],
+                three: [4000, 350, true],
+                four: [6000, 380, true],
+                five: [8000, 340, true]
+              },
+        });
+
+        this.word = word;
+        this.x = x;
+
+        this.id = this.sound.play(word);
+        this.sound.pos(this.x, 0, 0, this.id);
     }
 
+    moveRight(){
+        this.x++;
+        this.sound.pos(this.x, 0, 0, this.id);
+    }
 
+    moveLeft(){
+        this.x--;
+        this.sound.pos(this.x, 0, 0, this.id);
+    }
+
+    pickUp(){
+        this.sound.volume(0);
+    }
+
+    
+    drop(){
+        this.sound.volume(1);
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,11 +1,97 @@
 
 let soundGrid = [];
+let currentPosition = 0;
+let pickedSoundBox = null;
 
+//split our sound into n sounds
+//setupSoundBoxes();
 
-/*
-var sound = new Howl({
-    src: ['sound.mp3']
-  });
-  
-  sound.play();
-  */
+function setupSoundBoxes(){
+    const words = ["two", "one", "three", "four", "five"];
+
+    const n = 5;
+    const spacing = 5;
+    
+    for(let i = 0;i<n;i++){
+    
+        let tempSoundBox = new SoundBox(i*spacing, words[i]);
+    
+        soundGrid.push(tempSoundBox);
+    }
+}
+
+function moveRight(){
+    for(let i = 0; i <soundGrid.length;i++){
+        soundGrid[i].moveLeft();
+    }
+
+    currentPosition++;
+
+    if(pickedSoundBox != null){
+        pickedSoundBox.moveRight();
+    }
+}
+
+function moveLeft(){
+    for(let i = 0; i <soundGrid.length;i++){
+        soundGrid[i].moveRight();
+    }
+
+    currentPosition--;
+
+    if(pickedSoundBox != null){
+        pickedSoundBox.moveLeft();
+    }
+}
+
+function closestBoxIndex(){
+    let closestDis = 1000;
+    let closestIndex = 0;
+
+    for(let i = 0;i<soundGrid.length;i++){
+        let dis = Math.abs(soundGrid[i].x - currentPosition);
+
+        if(dis < closestDis){
+            closestDis = dis;
+            closestIndex = i;
+        }
+    }
+
+    return closestIndex
+}
+
+function pickupBox(){
+    pickedSoundBox = soundGrid.splice(closestBoxIndex(), 1)[0];
+    pickedSoundBox.pickUp();
+    console.log(pickedSoundBox);
+}
+
+function dropBox(){
+    pickedSoundBox.drop();
+    pickedSoundBox.x = currentPosition;
+    soundGrid.push(pickedSoundBox);
+    pickedSoundBox = null;
+}
+
+document.onkeydown = function (e) {
+    e = e || window.event;
+
+    if(e.keyCode == '37') {
+       // left arrow
+        console.log("left");
+
+        moveLeft();
+    }
+    else if(e.keyCode == '39') {
+       // right arrow
+        console.log("right");
+
+        moveRight();   
+    }
+    else if(e.keyCode == '32'){
+        console.log("space");
+
+        if(pickedSoundBox != null) dropBox();
+        else pickupBox();
+    }
+}
